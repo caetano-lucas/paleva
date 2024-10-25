b449f7634537a345e9bf84140e94a2798214175f
Lucas Soares Caetano <caetanolucassoares@gmail.com>
2024-10-25 17:14:58 -0300

    organiza specs

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
-class Restaurant < ApplicationRecord
+class Restaurant < ApplicationRecord  
+  validates :trade_name, :legal_name, :cnpj, :address, :phone, :email, presence: true
+  validates :legal_name, :cnpj, :address, :phone, :email, uniqueness: true
+  validates :cnpj, cnpj: true
 end
