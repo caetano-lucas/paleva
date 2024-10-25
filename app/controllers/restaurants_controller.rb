8cc7e7c99ab7b65906e24f9e60550737567a7262
Lucas Soares Caetano <caetanolucassoares@gmail.com>
2024-10-25 19:15:09 -0300

    cria restaurante

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,3 +1,21 @@
 class RestaurantsController < ApplicationController
-  
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to root_path, notice: "Restaurante cadastrado com sucesso"
+    else
+      flash.now[ :notice ] = "ALGO DEU ERRADO, RESTAURANTE NÃO CADASTRADO"
+      render 'new'
+    end
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:trade_name, :legal_name, :cnpj,
+    :address, :phone, :email)
+  end
 end
---
+++ spec/system/restaurants/user_register_restaurants_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+require 'cpf_cnpj'
+
+describe 'Usuário registra um restaurante' do
+  it 'com sucesso' do 
+    cpf = CPF.generate(true)
+    cnpj = CNPJ.generate(true)
+    User.create!(email: 'userone@email.com',first_name: 'userone',
+                 last_name: 'one', password: '12345abcdeF#', cpf: cpf )
+    visit root_path
+    within('nav') do
+      click_on 'Entrar'
+    end
+    within('form') do
+      fill_in 'E-mail', with: 'userone@email.com'
+      fill_in 'Senha', with: '12345abcdeF#'
+      click_on 'Entrar'
+    end
+    within('nav') do
+      click_on 'Cadastrar Restaurante'
+    end
+    fill_in 'Nome Fantasia', with: 'NomeFantasiaTeste'
+    fill_in 'Razão Social', with: 'RazãoSocialTeste LTDA'
+    fill_in 'Telefone', with: 'NomeFantasiaTeste'
+    fill_in 'E-mail', with: 'NomeFantasiaTeste@gmail.com'
+    fill_in 'Endereço', with: 'Rua do restauranteTeste, número: 200'
+    fill_in 'CNPJ', with: cnpj
+    click_on 'Salvar Restaurante'
+
+    expect(page).to have_content 'Restaurante cadastrado com sucesso'
+  end
+
+end
