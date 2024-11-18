c3973450b4fb56cf76ceb80b0b4798b1ec9d8363
Lucas Soares Caetano <caetanolucassoares@gmail.com>
2024-11-18 20:42:58 -0300

    corrige teste

--- spec/requests/apis/order_api_spec.rb
+++ spec/requests/apis/order_api_spec.rb
@@ -116,34 +116,5 @@ describe 'Orders API' do
 
   end
 
-  context 'PATCH api/v1/restaurant/1/orders/1' do
-    it 'success' do 
-      cpf = CPF.generate(true).split
-      cpf2 = CPF.generate(true).split
-      cnpj = CNPJ.generate(true).split
-      user = User.create!(email: 'userone@email.com',first_name: 'userone',
-                  last_name: 'one', password: '12345abcdeF#', cpf: cpf)
-      restaurant = Restaurant.create!(trade_name: 'userone-restaurant', legal_name: 'userRestaurant LTDA',
-                                      cnpj: cnpj, address: 'Restaurant street, 200', phone: '23456789102',
-                                      email: 'useronerestaurant@gmail.com')
-      user.update!(restaurant_id: restaurant.id, position: :owner)
-      dish = Dish.create!(name: 'Macarronada', description: 'Carne - provolone', calories: 1800, restaurant: restaurant )
-      portion1 = Portion.create!(description: 'Meia porção - 3 pessoas', price_whole: 45, price_cents: 99, portionable_type: 'Dish', portionable_id: dish.id)
-
-      order = Order.create!(client_name: 'Felipe Marciel', cpf: "#{cpf2}", phone: nil, email: 'felipemarciel@exemple.com',
-                    restaurant: restaurant, status: 0)
-      
-      order_item = OrderItem.create!(portion: portion1, order: order, quantity: 2, price: portion1.price_whole.to_i, cents: portion1.price_cents.to_i, note: 'sem cebola')
-      order.update!(total_price: 30.15)
-      
-      patch "/api/v1/restaurants/#{restaurant.alphanumeric_code}/orders/#{order.alphanumeric_code}?status=3"
-
-      expect(response.status).to eq 200
-      expect(response.content_type).to include 'application/json'
-
-      json_response = JSON.parse(response.body)
-      expect(json_response['order']["status"]).to eq('pronto')
-    end
-
-  end
+  
 end
